445d7288c87c7f3bb403d4c4e1c100fe98725c00
Martin Werner <m.werner.0@protonmail.com>
2023-12-19 20:26:03 +0100

    Change timing_tubaf to communicate a number and measure the elapsed time

--- src/timing_tubaf/scripts/my_subscriber.py
+++ src/timing_tubaf/scripts/my_subscriber.py
@@ -1,52 +1,49 @@
 #!/usr/bin/env python3
 
-# Copyright 2016 Open Source Robotics Foundation, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import time
 
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from std_msgs.msg import UInt32
+from std_msgs.msg import Float32
+
 
 
 class MinimalSubscriber(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
+        super().__init__('number_subscriber')
+
+        # subscriber for 'number' topic
         self.subscription = self.create_subscription(
-            String,
-            'topic',
-            self.listener_callback,
-            10)
-        self.subscription  # prevent unused variable warning
+            UInt32, 'number', self.listener_callback, 10)
+        self.subscription               # prevent unused variable warning
+
+        # publisher for elapsed time
+        self.diff_publisher = self.create_publisher(Float32, 'diff', 10)
+        self.timepoint = time.time()    # object to save timepoint of previous message
+
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        # measure and publish elapsed time
+        now = time.time()
+        diff = Float32()
+        diff.data = now - self.timepoint
+        self.diff_publisher.publish(diff)
+        self.timepoint = now
+
+        self.get_logger().info('Received: "%i" after %.5f s' % (msg.data, diff.data))
+
 
 
 def main(args=None):
     rclpy.init(args=args)
-
     minimal_subscriber = MinimalSubscriber()
-
     rclpy.spin(minimal_subscriber)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     minimal_subscriber.destroy_node()
     rclpy.shutdown()
+
 
 
 if __name__ == '__main__':
